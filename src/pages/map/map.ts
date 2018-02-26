7ce0a47a19636a0763f4ca75d74fa6c46ebcd96d
estrangegit <etienne.estrangin@gmail.com>
2018-02-27 00:14:55 +0100

    Ajout d'un onglet Map

---
+++ src/pages/map/map.ts
@@ -0,0 +1,75 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {UserDataServiceProvider} from '../../providers/user-data-service/user-data-service';
+import { Geolocation } from '@ionic-native/geolocation';
+import {App} from 'ionic-angular';
+import {LoginPage} from '../login/login';
+
+declare var google;
+
+@Component({
+  selector: 'page-map',
+  templateUrl: 'map.html'
+})
+
+export class MapPage {
+
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+
+  constructor(public userDataServiceProvider: UserDataServiceProvider,
+              public app: App,
+              public geolocation: Geolocation) {}
+
+  ionViewCanEnter(): boolean {
+    let loggedIn = this.userDataServiceProvider.isLoggedIn();
+    let disconnectedMode = this.userDataServiceProvider.isDisconnectedMode();
+    if(!loggedIn && !disconnectedMode){
+      this.app.getRootNav().setRoot(LoginPage);
+      return false;
+    }else{
+      return true;
+    }
+  }
+
+  ionViewDidLoad(){
+    this.loadMap();
+  }
+
+  loadMap(){
+
+    this.geolocation.getCurrentPosition().then((position) => {
+
+      let latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+
+      let mapOptions = {
+        center: latLng,
+        zoom: 15,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+
+      this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }, (err) => {
+      console.log(err);
+    });
+  }
+
+  addMarker(){
+    let marker = new google.maps.Marker({
+      map: this.map,
+      animation: google.maps.Animation.DROP,
+      position: this.map.getCenter()
+    });
+    let content = "<h4>Information!</h4>";
+    this.addInfoWindow(marker, content);
+  }
+
+  addInfoWindow(marker, content){
+    let infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+    google.maps.event.addListener(marker, 'click', () => {
+      infoWindow.open(this.map, marker);
+    });
+  }
+}
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { TodoListPage } from '../todolist/todolist';
 import { HomePage } from '../home/home';
 import {ProfilePage} from '../profile/profile';
+import {MapPage} from '../map/map';
 
 @Component({
   templateUrl: 'tabs.html'
@@ -12,6 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = TodoListPage;
   tab3Root = ProfilePage;
+  tab4Root = MapPage;
 
   constructor() {}
 }
